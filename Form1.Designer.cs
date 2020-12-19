453f586779393f6dc8ebe13d9e38df680ca9dc4a
jeads <60721703+jeads92@users.noreply.github.com>
2020-12-19 13:00:02 -0500

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace AlgoVisualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.PrimeButton = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // PrimeButton
+            // 
+            this.PrimeButton.Location = new System.Drawing.Point(113, 13);
+            this.PrimeButton.Name = "PrimeButton";
+            this.PrimeButton.Size = new System.Drawing.Size(250, 23);
+            this.PrimeButton.TabIndex = 1;
+            this.PrimeButton.Text = "Press to add to lsit";
+            this.PrimeButton.UseVisualStyleBackColor = true;
+            this.PrimeButton.Click += new System.EventHandler(this.BigClicken);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(16, 53);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(347, 20);
+            this.textBox.TabIndex = 2;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(16, 95);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(347, 95);
+            this.listBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 359);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.PrimeButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Names";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button PrimeButton;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlgoVisualizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BigClicken(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox.Text) && !listBox.Items.Contains(textBox))
+            {
+                listBox.Items.Add(textBox.Text);
+            }
+        }
+    }
+}
