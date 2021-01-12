67c3033804628d3a35a056dc1d6eb549d5b458c3
jeads <60721703+jeads92@users.noreply.github.com>
2021-01-12 12:07:02 -0500

    Rewrote part of the method that draws the rectangles so it is easier to experiment with them.

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace AlgoVisualizer
 {
@@ -17,77 +18,29 @@ namespace AlgoVisualizer
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        public void DrawRectangleInt(PaintEventArgs e)
-        {
-
-            // Create pen.
-            Pen blackPen = new Pen(Color.Black, 3);
-
-            // Create location and size of rectangle.
-            int x = 0;
-            int y = 0;
-            int width = 200;
-            int height = 200;
-
-            // Draw rectangle to screen.
-            e.Graphics.DrawRectangle(blackPen, x, y, width, height);
-        }
-
-    }
-
-    public class VisualizerControl : Control
-    {
-        private Color[] _colors;
-        public Color[] Colors
-        {
-            get
-            {
-                return _colors;
-            }
-            set
-            {
-                _colors = value;
-                // Redraw if the array is changed to a new one.
-                Invalidate();
-            }
-        }
-
         protected override void OnPaint(PaintEventArgs e)
         {
-            var g = e.Graphics;
-
-            // Draw the background.
-            g.Clear(BackColor);
-
-            // If the array's null or too small, don't try to draw it.
-            if (Colors == null || Colors.Length == 0)
-            {
-                return;
-            }
-
-            // Start drawing squares at (10, 10), moving horizontally
-            // for each one, with 5px between them.
+            int[] testArray = new int[] { 250, 500 };
             var x = 10;
             var y = 10;
             var squareSide = 20;
-            var buffer = 5;
+            var buffer = 20;
+            base.OnPaint(e);
+            SolidBrush myBrush = new SolidBrush(Color.Green);
+            SolidBrush myBrush2 = new SolidBrush(Color.Red);
 
-            foreach (var color in Colors)
-            {
-                using (var b = new SolidBrush(color))
-                {
-                    g.FillRectangle(b, x, y, squareSide, squareSide);
+            e.Graphics.FillRectangle(myBrush2, new Rectangle(0, 0, 200, 100));
+            e.Graphics.FillRectangle(myBrush, new Rectangle(700, 10, 200, 10));
+        }
 
-                    // "move" to the next square
-                    x += squareSide + buffer;
-                }
-            }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            SolidBrush myBrush = new SolidBrush(Color.Green);
+            Graphics formGraphics;
+            formGraphics = this.CreateGraphics();
+            formGraphics.FillRectangle(myBrush, new Rectangle(0, 0, 200, 300));
+            myBrush.Dispose();
+            formGraphics.Dispose();
         }
     }
-
 }
