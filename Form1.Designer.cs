258759dd77d8f427381716634b47ba767346bb61
jeads <60721703+jeads92@users.noreply.github.com>
2021-03-01 17:13:08 -0500

    Added Insertion sort to the program, a debugger to see if array is properly being sorted.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,41 +29,32 @@ namespace AlgoVisualizer
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.Button2 = new System.Windows.Forms.Button();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.trackbarValueLabel = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.buttonSort = new System.Windows.Forms.Button();
+            this.intTestLabel = new System.Windows.Forms.Label();
+            this.createArray = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Button2
-            // 
-            this.Button2.Location = new System.Drawing.Point(131, 439);
-            this.Button2.Name = "Button2";
-            this.Button2.Size = new System.Drawing.Size(122, 23);
-            this.Button2.TabIndex = 0;
-            this.Button2.Text = "Start Algorithm";
-            this.Button2.UseVisualStyleBackColor = true;
-            this.Button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(16, 12);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Data";
-            this.chart1.Series.Add(series2);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Data";
+            this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(445, 395);
             this.chart1.TabIndex = 3;
             this.chart1.Text = "chart1";
@@ -71,7 +62,7 @@ namespace AlgoVisualizer
             // trackBar1
             // 
             this.trackBar1.Location = new System.Drawing.Point(12, 439);
-            this.trackBar1.Maximum = 500;
+            this.trackBar1.Maximum = 100;
             this.trackBar1.Minimum = 1;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(104, 45);
@@ -106,16 +97,37 @@ namespace AlgoVisualizer
             this.buttonSort.TabIndex = 7;
             this.buttonSort.Text = "Start Sort";
             this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // intTestLabel
+            // 
+            this.intTestLabel.AutoSize = true;
+            this.intTestLabel.Location = new System.Drawing.Point(366, 89);
+            this.intTestLabel.Name = "intTestLabel";
+            this.intTestLabel.Size = new System.Drawing.Size(35, 13);
+            this.intTestLabel.TabIndex = 8;
+            this.intTestLabel.Text = "label1";
+            // 
+            // createArray
+            // 
+            this.createArray.Location = new System.Drawing.Point(137, 439);
+            this.createArray.Name = "createArray";
+            this.createArray.Size = new System.Drawing.Size(75, 23);
+            this.createArray.TabIndex = 9;
+            this.createArray.Text = "Create Array";
+            this.createArray.UseVisualStyleBackColor = true;
+            this.createArray.Click += new System.EventHandler(this.createArray_Click);
             // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(473, 474);
+            this.Controls.Add(this.createArray);
+            this.Controls.Add(this.intTestLabel);
             this.Controls.Add(this.buttonSort);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.trackbarValueLabel);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.chart1);
-            this.Controls.Add(this.Button2);
             this.Name = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
@@ -127,12 +139,13 @@ namespace AlgoVisualizer
         #endregion
         private System.Windows.Forms.BindingSource bindingSource1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button Button2;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.Label trackbarValueLabel;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.Label intTestLabel;
+        private System.Windows.Forms.Button createArray;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,16 +16,56 @@ namespace AlgoVisualizer
 {
     public partial class Form1 : Form
     {
+        protected int[] _dataSet = new int[100];
+
         public Form1()
         {
             InitializeComponent();
+            string arrayText = "";
+            foreach(int element in _dataSet)
+            {
+                arrayText += element;
+                arrayText += ",";
+            }
+            intTestLabel.Text = arrayText;
         }
-
         // Initializes size of the array.
         public int arraySize;
 
+        
+
+
+        // updates the label to show the current value that trackBar1 has selected.
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            trackbarValueLabel.Text = trackBar1.Value.ToString();
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            //Array.Sort(_dataSet);
+
+            Sorting.Algorithms.InsertionSort(_dataSet);
+            string arrayText = "";
+            foreach (int element in _dataSet)
+            {
+                arrayText += element;
+                arrayText += ",";
+            }
+            intTestLabel.Text = arrayText;
+
+            chart1.Series["Data"].Points.Clear();
+            int placement = 0;
+            foreach (int dataPoint in _dataSet)
+            {
+                this.chart1.Series["Data"].Points.AddXY(placement, dataPoint);
+                placement += 1;
+            }
+
+        }
+
         // Generates the array and links it to the chart for visualization.
-        private void button2_Click(object sender, EventArgs e)
+        private void createArray_Click(object sender, EventArgs e)
         {
             // the dataArray takes in the arraySize and returns an array
             // which is used to populate the chart.
@@ -33,19 +73,40 @@ namespace AlgoVisualizer
             ArrayObject dataGenerator = new ArrayObject();
             int[] dataArray = dataGenerator.GenerateData(arraySize);
 
+            int replaceIndex = 0;
+            foreach(int replacePoint in _dataSet)
+            {
+                _dataSet[replaceIndex] = 0;
+                replaceIndex += 1;
+            }
+
+            // updates the primary array with the randomly generated values.
+            int count = 0;
+            foreach(int point in dataArray)
+            {
+                _dataSet[count] = point;
+                count += 1;
+            }
+
+            // clears the chart and updates it with the new array.
             chart1.Series["Data"].Points.Clear();
             int placement = 0;
-            foreach (int dataPoint in dataArray)
+            foreach (int dataPoint in _dataSet)
             {
                 this.chart1.Series["Data"].Points.AddXY(placement, dataPoint);
                 placement += 1;
             }
-        }
 
-        // updates the label to show the current value that trackBar1 has selected.
-        private void trackBar1_Scroll(object sender, EventArgs e)
-        {
-            trackbarValueLabel.Text = trackBar1.Value.ToString();
+            // updates the label. This will be removed later. For debugging purposes only.
+            string arrayText = "";
+            foreach (int element in _dataSet)
+            {
+                arrayText += element;
+                arrayText += ",";
+            }
+            intTestLabel.Text = arrayText;
+
+
         }
     }
 
@@ -59,7 +120,6 @@ namespace AlgoVisualizer
             {
                 dataArray[i] = number.Next(0, 99);
             }
-
             return dataArray;
         }
     }
@@ -68,24 +128,23 @@ namespace AlgoVisualizer
 
 namespace Sorting
 {
-    class Algorithms
+    public class Algorithms
     {
-        static void InsertionSort(int[] intArray)
+        public static void InsertionSort(int[] intArray)
         {
             foreach (int number in intArray)
             {
                 Console.Write(number);
                 Console.Write(", ");
             }
+
             Console.WriteLine();
             int i = 1;
             int j = 1;
             int placeHolder = 1;
-
             while (i < intArray.Length)
             {
                 j = i;
-
                 while (j > 0 && intArray[j - 1] > intArray[j])
                 {
                     placeHolder = intArray[j];
