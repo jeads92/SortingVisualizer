13badfb8ff3674a51c6babce0a62f054fc1dca92
jeads <60721703+jeads92@users.noreply.github.com>
2021-04-15 16:31:31 -0400

    Edited variable names and other code to fit with c# coding standards

--- Form1.cs
+++ Form1.cs
@@ -7,8 +7,9 @@ namespace AlgoVisualizer
     public partial class Form1 : Form
     {
         // Initializes size of the array.
-        public int arraySize;
-        public bool stopStatus = false;
+        public int _arraySize;
+        public int _countdown = 1;
+        public bool _stopStatus = false;
 
         public Form1()
         {
@@ -16,7 +17,7 @@ namespace AlgoVisualizer
   
         }
 
-        public void updateBarChart(int[] selectedArray)
+        public void UpdateBarChart(int[] selectedArray)
         {
             chart1.Series["Data"].Points.Clear();
             int placement = 0;
@@ -32,12 +33,72 @@ namespace AlgoVisualizer
             trackbarValueLabel.Text = trackBar1.Value.ToString();
         }
 
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            buttonSort.Enabled = false;
+            _stopStatus = false;
+            _arraySize = trackBar1.Value;
+            NumberGenerator dataRandomizer = new NumberGenerator();
+            int[] dataArray = dataRandomizer.Fill(_arraySize);
+            UpdateBarChart(dataArray);
+            // Tests to see if the selection box has been selected.
+            if (algorithmBox.SelectedItem is null)
+            {
+                return;
+            }
+            else
+            {
+                switch (algorithmBox.SelectedItem.ToString())
+                {
+
+                    case "Insertion Sort":
+                        InsertionSort(dataArray);
+                        break;
+                    case "Bubble Sort":
+                        BubbleSort(dataArray);
+                        break;
+                    case "Cocktail Shaker Sort":
+                        CocktailSort(dataArray);
+                        break;
+                    case "Quick Sort":
+                        int arrayLength = dataArray.Length - 1;
+                        QuickSort(dataArray, 0, arrayLength);
+                        break;
+                    case null:
+                        break;
+                }
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            _stopStatus = true;
+            buttonSort.Enabled = true;
+        }
+
+        private void testTimer_Tick(object sender, EventArgs e)
+        {
+            if (_countdown > 0)
+            {
+                _countdown -= 1;
+                Console.WriteLine("_countdown subtractions");
+            }
+            else
+            {
+                testTimer.Stop();
+                Console.WriteLine("timer stopped");
+                _countdown = 1;
+                return;
+            }
+
+        }
+
         public async void InsertionSort(int[] intArray)
         {
             int i = 1;
             int j = 1;
             int placeHolder = 1;
-            while (i < intArray.Length && stopStatus == false)
+            while (i < intArray.Length && _stopStatus == false)
             {
                 j = i;
                 while (j > 0 && intArray[j - 1] > intArray[j])
@@ -46,7 +107,7 @@ namespace AlgoVisualizer
                     intArray[j] = intArray[j - 1];
                     intArray[j - 1] = placeHolder;
                     j--;
-                    updateBarChart(intArray);
+                    UpdateBarChart(intArray);
                     await Task.Delay(sortSpeedBar.Value);
                 }
                 i++;
@@ -60,12 +121,12 @@ namespace AlgoVisualizer
             {
                 for (int j = 0; j < n - i - 1; j++)
                 {
-                    if (intArray[j] > intArray[j + 1] && stopStatus == false)
+                    if (intArray[j] > intArray[j + 1] && _stopStatus == false)
                     {
                         int placeHolder = intArray[j];
                         intArray[j] = intArray[j + 1];
                         intArray[j + 1] = placeHolder;
-                        updateBarChart(intArray);
+                        UpdateBarChart(intArray);
                         
                         await Task.Delay(sortSpeedBar.Value);
                     }
@@ -85,13 +146,13 @@ namespace AlgoVisualizer
 
                 for (int i = start; i < end - 1; ++i)
                 {
-                    if (intArray[i] > intArray[i + 1] & stopStatus == false)
+                    if (intArray[i] > intArray[i + 1] & _stopStatus == false)
                     {
                         int placeHolder = intArray[i];
                         intArray[i] = intArray[i + 1];
                         intArray[i + 1] = placeHolder;
                         swapped = true;
-                        updateBarChart(intArray);
+                        UpdateBarChart(intArray);
                         
                         await Task.Delay(sortSpeedBar.Value);
                     }
@@ -104,13 +165,13 @@ namespace AlgoVisualizer
 
                 for (int i = end - 1; i >= start; i--)
                 {
-                    if (intArray[i] > intArray[i + 1] && stopStatus == false)
+                    if (intArray[i] > intArray[i + 1] && _stopStatus == false)
                     {
                         int temp = intArray[i];
                         intArray[i] = intArray[i + 1];
                         intArray[i + 1] = temp;
                         swapped = true;
-                        updateBarChart(intArray);
+                        UpdateBarChart(intArray);
                         
                         await Task.Delay(sortSpeedBar.Value);
                     }
@@ -119,71 +180,12 @@ namespace AlgoVisualizer
             }
         }
 
-        public async void heapSort(int[] intArray)
-        {
-            int n = intArray.Length;
-
-            // This builds the heap and rearranges the array.
-            // n / 2 - 1 focuses on heapifying all nodes except the leaf nodes.
-            for (int i = n / 2 - 1; i >= 0; i--)
-            {
-                heapify(intArray, n, i);
-                updateBarChart(intArray);
-
-                await Task.Delay(sortSpeedBar.Value);
-            }
-
-            // This extracts an element from the heap.
-            for (int i = n - 1; i > 0; i--)
-            {
-                // This moves the current root to end.
-                int temp = intArray[0];
-                intArray[0] = intArray[i];
-                intArray[i] = temp;
-                updateBarChart(intArray);
-
-                await Task.Delay(sortSpeedBar.Value);
-
-                // This calls max heapify on the reduced heap.
-                heapify(intArray, i, 0);
-            }
-        }
-
-        // To heapify a subtree with node i as the root
-        // n = size of the heap.
-        public void heapify(int[] arr, int n, int i)
-        {
-            int largestNode = i; // Initialize largest as root
-            int left = 2 * i + 1; // left = 2*i + 1
-            int right = 2 * i + 2; // right = 2*i + 2
-
-            // Replaces the largest value as the left node.
-            if (left < n && arr[left] > arr[largestNode])
-                largestNode = left;
-
-            // Replaces the largest value as the right node.
-            if (right < n && arr[right] > arr[largestNode])
-                largestNode = right;
-
-            // This updates the value for the largest node
-            // if there was a swap with one of the child nodes.
-            if (largestNode != i)
-            {
-                int swap = arr[i];
-                arr[i] = arr[largestNode];
-                arr[largestNode] = swap;
-
-                // Recursively heapify any sub trees that
-                // underwent a change.
-                heapify(arr, n, largestNode);
-            }
-        }
-
-        public void swap(int[] arr, int i, int j)
+        public void Swap(int[] arr, int i, int j)
         {
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
+            
         }
 
         /* This function takes last element as a pivot, places
@@ -191,7 +193,7 @@ namespace AlgoVisualizer
         array, and places all smaller (smaller than pivot)
         to left of pivot and all greater elements to right
         of pivot */
-        public int partition(int[] arr, int low, int high)
+        public int Partition(int[] arr, int low, int high)
         {
             // pivot
             int pivot = arr[high];
@@ -212,12 +214,10 @@ namespace AlgoVisualizer
                     // Increment index of
                     // smaller element
                     i++;
-                    swap(arr, i, j);
-                    testTimer.Start();
+                    Swap(arr, i, j);
                 }
             }
-            swap(arr, i + 1, high);
-            testTimer.Start();
+            Swap(arr, i + 1, high);
             return (i + 1);
         }
 
@@ -226,77 +226,21 @@ namespace AlgoVisualizer
         low --> Starting index,
         high --> Ending index
         */
-        public async void quickSort(int[] arr, int low, int high)
+        public void QuickSort(int[] arr, int low, int high)
         {
             if (low < high)
             {
 
                 // pi is partitioning index, arr[p]
                 // is now at right place
-                int pi = partition(arr, low, high);
+                int pi = Partition(arr, low, high);
 
                 // Separately sort elements before
                 // partition and after partition
-                quickSort(arr, low, pi - 1);
-                quickSort(arr, pi + 1, high);
-                updateBarChart(arr);
-                await Task.Delay(sortSpeedBar.Value);
+                QuickSort(arr, low, pi - 1);
+                QuickSort(arr, pi + 1, high);
+                UpdateBarChart(arr);
             }
-        }
-
-        private void buttonSort_Click(object sender, EventArgs e)
-        {
-            buttonSort.Enabled = false;
-            stopStatus = false;
-            arraySize = trackBar1.Value;
-            NumberGenerator dataRandomizer = new NumberGenerator();
-            int[] dataArray = dataRandomizer.Fill(arraySize);
-            updateBarChart(dataArray);
-            // Tests to see if the selection box has been selected.
-            if (algorithmBox.SelectedItem is null)
-            {
-                return;
-            }
-            else
-            {
-                switch (algorithmBox.SelectedItem.ToString())
-                {
-                    case "Insertion Sort":
-                        InsertionSort(dataArray);
-                        break;
-                    case "Bubble Sort":
-                        BubbleSort(dataArray);
-                        break;
-                    case "Cocktail Shaker Sort":
-                        CocktailSort(dataArray);
-                        break;
-                    case "Merge Sort":
-                        Sorting.Algorithms.MergeSort MergeObject = new Sorting.Algorithms.MergeSort();
-                        MergeObject.sort(dataArray, 0, dataArray.Length - 1);
-                        break;
-                    case "Quick Sort":
-                        int arrayLength = dataArray.Length - 1;
-                        quickSort(dataArray, 0, arrayLength);
-                        break;
-                    case "Heap Sort":
-                        heapSort(dataArray);
-                        break;
-                    case null:
-                        break;
-                }
-            }
-        }
-
-        private void stopButton_Click(object sender, EventArgs e)
-        {
-            stopStatus = true;
-            buttonSort.Enabled = true;
-        }
-
-        private void testTimer_Tick(object sender, EventArgs e)
-        {
-            Console.WriteLine("timer test");
-            testTimer.Stop();
         }
     }
 
@@ -313,7 +257,7 @@ namespace AlgoVisualizer
             return dataArray;
         }
 
-        static bool checkSort(int[] data, int arrayLength)
+        static bool CheckSort(int[] data, int arrayLength)
         {
             // Checks if the array is of size 0 or 1.
             if (arrayLength == 0 || arrayLength == 1)
@@ -334,7 +278,6 @@ namespace AlgoVisualizer
         }
 
     }
-
 }
 
 
@@ -343,84 +286,6 @@ namespace Sorting
 {
     public class Algorithms
     {
-
-        public class MergeSort
-        {
-            public void merge(int[] arr, int l, int m, int r)
-            {
-                // Sizes of the two array that will be merged.
-                int n1 = m - l + 1;
-                int n2 = r - m;
-
-                // Temporary Arrays
-                int[] L = new int[n1];
-                int[] R = new int[n2];
-                int i, j;
-
-                // Copy data to temp arrays
-                for (i = 0; i < n1; ++i)
-                    L[i] = arr[l + i];
-                for (j = 0; j < n2; ++j)
-                    R[j] = arr[m + 1 + j];
-
-                // Merge the temp arrays
-
-                // Initial indexes of the sub arrays
-                i = 0;
-                j = 0;
-
-                // Initial index of merged subarry array
-                int k = l;
-                while (i < n1 && j < n2)
-                {
-                    if (L[i] <= R[j])
-                    {
-                        arr[k] = L[i];
-                        i++;
-                    }
-                    else
-                    {
-                        arr[k] = R[j];
-                        j++;
-                    }
-                    k++;
-                }
-
-                // Copy remaining elements L
-                while (i < n1)
-                {
-                    arr[k] = L[i];
-                    i++;
-                    k++;
-                }
-
-                // Copy remaining elements R
-                while (j < n2)
-                {
-                    arr[k] = R[j];
-                    j++;
-                    k++;
-                }
-            }
-
-            // Primary sorting function.
-            public void sort(int[] arr, int l, int r)
-            {
-                if (l < r)
-                {
-                    // This find the middle point
-                    int m = l + (r - l) / 2;
-
-                    // This sorts the first and second halves
-                    sort(arr, l, m);
-                    sort(arr, m + 1, r);
-
-                    // Merges the sorted halves
-                    merge(arr, l, m, r);
-                }
-            }
-        }
-
         public class QuickSort
         {
             // A utility function to swap two elements
@@ -482,62 +347,6 @@ namespace Sorting
                     // partition and after partition
                     quickSort(arr, low, pi - 1);
                     quickSort(arr, pi + 1, high);
-                }
-            }
-
-        }
-
-        public class HeapSort
-        {
-            public void sort(int[] arr)
-            {
-                int n = arr.Length;
-
-                // This builds the heap and rearranges the array.
-                // n / 2 - 1 focuses on heapifying all nodes except the leaf nodes.
-                for (int i = n / 2 - 1; i >= 0; i--)
-                    heapify(arr, n, i);
-
-                // This extracts an element from the heap.
-                for (int i = n - 1; i > 0; i--)
-                {
-                    // This moves the current root to end.
-                    int temp = arr[0];
-                    arr[0] = arr[i];
-                    arr[i] = temp;
-
-                    // This calls max heapify on the reduced heap.
-                    heapify(arr, i, 0);
-                }
-            }
-
-            // To heapify a subtree with node i as the root
-            // n = size of the heap.
-            void heapify(int[] arr, int n, int i)
-            {
-                int largestNode = i; // Initialize largest as root
-                int left = 2 * i + 1; // left = 2*i + 1
-                int right = 2 * i + 2; // right = 2*i + 2
-
-                // Replaces the largest value as the left node.
-                if (left < n && arr[left] > arr[largestNode])
-                    largestNode = left;
-
-                // Replaces the largest value as the right node.
-                if (right < n && arr[right] > arr[largestNode])
-                    largestNode = right;
-
-                // This updates the value for the largest node
-                // if there was a swap with one of the child nodes.
-                if (largestNode != i)
-                {
-                    int swap = arr[i];
-                    arr[i] = arr[largestNode];
-                    arr[largestNode] = swap;
-
-                    // Recursively heapify any sub trees that
-                    // underwent a change.
-                    heapify(arr, n, largestNode);
                 }
             }
 
